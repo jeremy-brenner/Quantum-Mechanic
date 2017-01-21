facedaba8f7655cfd93e4606be2bad9ee54d0fb3
Jeremy J. Brenner <jeremyjbrenner@gmail.com>
2017-01-20 22:23:31 -0600

    simple render loop

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-console.log('test');
+
+var scene = new THREE.Scene();
+var camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 0, 1000 );
+
+camera.updateProjectionMatrix()
+camera.position.z = 100;
+
+var renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+var ambient = new THREE.AmbientLight( 0xffffff )
+scene.add(ambient);
+
+document.body.appendChild( renderer.domElement );
+
+var render = function () {
+  requestAnimationFrame( render );
+  renderer.render(scene, camera);
+};
+
+render();
